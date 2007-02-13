a225d3b54e8e3b3c86e52c5503ab6be08404d81f
Marcelo Roberto Jimenez <mroberto@users.sourceforge.net>
2007-02-13 13:33:15 +0000

    White spaces, reformating.
    git-svn-id: https://pupnp.svn.sourceforge.net/svnroot/pupnp/trunk@132 119443c7-1b9e-41f8-b6fc-b9c35fce742c

--- upnp/src/api/upnpapi.c
+++ upnp/src/api/upnpapi.c
@@ -3713,8 +3713,6 @@ FreeHandle( int Upnp_Handle )
 
 }  /****************** End of FreeHandle *********************/
 
-// **DBG****************************************************
-//DBGONLY(
 
 /**************************************************************************
  * Function: PrintHandleInfo 
@@ -3728,55 +3726,60 @@ FreeHandle( int Upnp_Handle )
  * Return Values: int
  *	UPNP_E_SUCCESS if successful else return appropriate error
  ***************************************************************************/
-            int PrintHandleInfo( IN UpnpClient_Handle Hnd ) {
-            struct Handle_Info * HndInfo; if( HandleTable[Hnd] != NULL ) {
-            HndInfo = HandleTable[Hnd];
-            DBGONLY( UpnpPrintf( UPNP_ALL, API, __FILE__, __LINE__,
-                                 "Printing information for Handle_%d\n",
-                                 Hnd );
-                     UpnpPrintf( UPNP_ALL, API, __FILE__, __LINE__,
-                                 "HType_%d\n", HndInfo->HType );
-		     DEVICEONLY(
-                     if( HndInfo->HType !=
-                         HND_CLIENT ) UpnpPrintf( UPNP_ALL, API, __FILE__,
-                                                  __LINE__, "DescURL_%s\n",
-                                                  HndInfo->DescURL ); )
-		     )
-            }
-            else
-            {
-            return UPNP_E_INVALID_HANDLE;}
+int PrintHandleInfo( IN UpnpClient_Handle Hnd )
+{
+    struct Handle_Info * HndInfo;
+    if (HandleTable[Hnd] != NULL) {
+        HndInfo = HandleTable[Hnd];
+        DBGONLY(
+            UpnpPrintf(UPNP_ALL, API, __FILE__, __LINE__,
+                "Printing information for Handle_%d\n", Hnd);
+            UpnpPrintf(UPNP_ALL, API, __FILE__, __LINE__,
+                "HType_%d\n", HndInfo->HType);
+            DEVICEONLY(
+                if(HndInfo->HType != HND_CLIENT)
+                    UpnpPrintf( UPNP_ALL, API, __FILE__, __LINE__,
+                        "DescURL_%s\n", HndInfo->DescURL );
+            )
+        )
+    } else {
+        return UPNP_E_INVALID_HANDLE;
+    }
 
-            return UPNP_E_SUCCESS;}
+    return UPNP_E_SUCCESS;
+}
+
    /****************** End of PrintHandleInfo *********************/
 
-            void printNodes( IXML_Node * tmpRoot, int depth ) {
-            int i;
-            IXML_NodeList * NodeList1;
-            IXML_Node * ChildNode1;
-            unsigned short NodeType;
-            const DOMString NodeValue;
-            const DOMString NodeName;
-            NodeList1 = ixmlNode_getChildNodes( tmpRoot );
-            for( i = 0; i < 100; i++ ) {
-            ChildNode1 = ixmlNodeList_item( NodeList1, i );
-            if( ChildNode1 == NULL ) {
-            break;}
+void printNodes( IXML_Node * tmpRoot, int depth )
+{
+    int i;
+    IXML_NodeList *NodeList1;
+    IXML_Node *ChildNode1;
+    unsigned short NodeType;
+    const DOMString NodeValue;
+    const DOMString NodeName;
+    NodeList1 = ixmlNode_getChildNodes(tmpRoot);
+    for (i = 0; i < 100; ++i) {
+        ChildNode1 = ixmlNodeList_item(NodeList1, i);
+        if (ChildNode1 == NULL) {
+            break;
+        }
+    
+        printNodes(ChildNode1, depth+1);
+        NodeType = ixmlNode_getNodeType(ChildNode1);
+        NodeValue = ixmlNode_getNodeValue(ChildNode1);
+        NodeName = ixmlNode_getNodeName(ChildNode1);
+        DBGONLY(
+            UpnpPrintf(UPNP_ALL, API, __FILE__, __LINE__,
+                "DEPTH-%2d-IXML_Node Type %d, "
+                "IXML_Node Name: %s, IXML_Node Value: %s\n",
+                depth, NodeType, NodeName, NodeValue);
+	)
+    }
+}
 
-            printNodes( ChildNode1, depth + 1 );
-            NodeType = ixmlNode_getNodeType( ChildNode1 );
-            NodeValue = ixmlNode_getNodeValue( ChildNode1 );
-            NodeName = ixmlNode_getNodeName( ChildNode1 );
-            DBGONLY( UpnpPrintf( UPNP_ALL, API, __FILE__, __LINE__,
-                                 "DEPTH-%2d-IXML_Node Type %d, "
-                                 "IXML_Node Name: %s, IXML_Node Value: %s\n",
-                                 depth, NodeType, NodeName, NodeValue ); )
-            }
-
-            }
    /****************** End of printNodes *********************/
-
-// )                              // dbgonly
 
     //********************************************************
     //* Name: getlocalhostname
