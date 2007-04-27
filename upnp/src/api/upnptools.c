c0d9908afe1c6db809ff99905b3436cf4f5d0904
Marcelo Roberto Jimenez <mroberto@users.sourceforge.net>
2007-04-27 11:42:26 +0000

    Undoing the fix for the fix.
    git-svn-id: https://pupnp.svn.sourceforge.net/svnroot/pupnp/trunk@164 119443c7-1b9e-41f8-b6fc-b9c35fce742c

--- upnp/src/api/upnptools.c
+++ upnp/src/api/upnptools.c
@@ -259,7 +259,7 @@ makeAction( IN int response,
     const char *ArgName;
     const char *ArgValue;
     char *ActBuff;
-    int Idx;
+    int Idx = 0;
     IXML_Document *ActionDoc;
     IXML_Node *node;
     IXML_Element *Ele;
@@ -298,7 +298,7 @@ makeAction( IN int response,
     if( NumArg > 0 ) {
         //va_start(ArgList, Arg);
         ArgName = Arg;
-        for(Idx = 0; Idx < NumArg; ++Idx) {
+        for ( ; ; ) {
             ArgValue = va_arg( ArgList, const char * );
 
             if( ArgName != NULL ) {
@@ -313,8 +313,10 @@ makeAction( IN int response,
                 ixmlNode_appendChild( node, ( IXML_Node * ) Ele );
             }
 
-            if ( Idx != NumArg - 1) {
+            if (++Idx < NumArg) {
                 ArgName = va_arg( ArgList, const char * );
+            } else {
+                break;
             }
         }
         //va_end(ArgList);
