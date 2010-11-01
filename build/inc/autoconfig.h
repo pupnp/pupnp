d6671c464f231adda5d4c853740724e24d95e2ca
Marcelo Roberto Jimenez <mroberto@users.sourceforge.net>
2010-11-01 01:00:26 -0200

    Bump config to 1.6.9.

--- build/inc/autoconfig.h
+++ build/inc/autoconfig.h
@@ -105,13 +105,13 @@
 #define PACKAGE_NAME "libupnp"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libupnp 1.6.8"
+#define PACKAGE_STRING "libupnp 1.6.9"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "libupnp"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.6.8"
+#define PACKAGE_VERSION "1.6.9"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
@@ -151,13 +151,13 @@
 #define UPNP_VERSION_MINOR 6
 
 /* see upnpconfig.h */
-#define UPNP_VERSION_PATCH 8
+#define UPNP_VERSION_PATCH 9
 
 /* see upnpconfig.h */
-#define UPNP_VERSION_STRING "1.6.8"
+#define UPNP_VERSION_STRING "1.6.9"
 
 /* Version number of package */
-#define VERSION "1.6.8"
+#define VERSION "1.6.9"
 
 /* File Offset size */
 #define _FILE_OFFSET_BITS 64
