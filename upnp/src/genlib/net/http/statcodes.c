1a1570fe0fa95bfc80b74de65e80dff9b7565b9e
Fabrice Fontaine <fabrice.fontaine@orange.com>
2012-03-06 00:13:46 +0100

    SF Bug Tracker id 3497140 - Bug fix in http_get_code_text
    Submitted: Fabrice Fontaine ( ffontaine ) - 2012-03-05 13:07:03 PST
    
    Replace if( statusCode < 100 && statusCode >= 600 ) which can't be true
    by if( statusCode < 100 || statusCode >= 600 ).

--- upnp/src/genlib/net/http/statcodes.c
+++ upnp/src/genlib/net/http/statcodes.c
@@ -2,6 +2,7 @@
  *
  * Copyright (c) 2000-2003 Intel Corporation
  * All rights reserved.
+ * Copyright (c) 2012 France Telecom All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -179,7 +180,7 @@ http_get_code_text( int statusCode )
         init_tables();
     }
 
-    if( statusCode < 100 && statusCode >= 600 ) {
+    if( statusCode < 100 || statusCode >= 600 ) {
         return NULL;
     }
 
