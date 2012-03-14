05fb3f80262faef3de496b9f1629274dc883eab5
Fabrice Fontaine <fabrice.fontaine@orange.com>
2012-03-14 18:43:14 +0100

    Fix missing break in http_RecvMessage
    There was a missing break in PARSE_INCOMPLETE_ENTITY due to commit
    2eb3e069badd5c8676738c3ead37f9551fd8448e.

--- upnp/src/genlib/net/http/httpreadwrite.c
+++ upnp/src/genlib/net/http/httpreadwrite.c
@@ -312,6 +312,7 @@ int http_RecvMessage(
 			case PARSE_INCOMPLETE_ENTITY:
 				/* read until close */
 				ok_on_close = TRUE;
+				break;
 			case PARSE_CONTINUE_1:
 				/* Web post request. */
 				line = __LINE__;
