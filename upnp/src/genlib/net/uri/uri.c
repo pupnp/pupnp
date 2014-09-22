pupnp/pupnp

commit 0edaf3361db01425cae0daee7dc3f6039f381a17

Remove most of strcpy, sprintf and strcat
Replace strcpy, sprintf and strcat by strncpy, snprintf and strncat to
avoid buffer overflows.

(forward port of commit 97a17ff5add73c97844e2fa74456bab4df0800f1)

upnp/src/genlib/net/uri/uri.c
@@ -2,6 +2,7 @@
  *
  * Copyright (c) 2000-2003 Intel Corporation 
  * All rights reserved. 
+ * Copyright (c) 2012 France Telecom All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met: 
@@ -589,12 +590,13 @@ char *resolve_rel_url(char *base_url, char *rel_url)
     if( out == NULL ) {
         return NULL;
     }
+    memset( out, 0, strlen( base_url ) + strlen( rel_url ) + 2 );
 
     if( ( parse_uri( rel_url, strlen( rel_url ), &rel ) ) == HTTP_SUCCESS ) {
 
         if( rel.type == ABSOLUTE ) {
 
-            strcpy( out, rel_url );
+            strncpy( out, rel_url, strlen ( rel_url ) );
         } else {
 
             if( ( parse_uri( base_url, strlen( base_url ), &base ) ==
@@ -602,7 +604,7 @@ char *resolve_rel_url(char *base_url, char *rel_url)
                 && ( base.type == ABSOLUTE ) ) {
 
                 if( strlen( rel_url ) == 0 ) {
-                    strcpy( out, base_url );
+                    strncpy( out, base_url, strlen ( base_url ) );
                 } else {
                     memcpy( out, base.scheme.buff, base.scheme.size );
                     out_finger += base.scheme.size;
@@ -610,7 +612,7 @@ char *resolve_rel_url(char *base_url, char *rel_url)
                     out_finger++;
 
                     if( rel.hostport.text.size > 0 ) {
-                        sprintf( out_finger, "%s", rel_url );
+                        snprintf( out_finger, strlen( rel_url ), "%s", rel_url );
                     } else {
                         if( base.hostport.text.size > 0 ) {
                             memcpy( out_finger, "//", 2 );
@@ -621,7 +623,7 @@ char *resolve_rel_url(char *base_url, char *rel_url)
                         }
 
                         if( rel.path_type == ABS_PATH ) {
-                            strcpy( out_finger, rel_url );
+                            strncpy( out_finger, rel_url, strlen ( rel_url ) );
 
                         } else {
 
@@ -642,7 +644,7 @@ char *resolve_rel_url(char *base_url, char *rel_url)
                                 finger++;
 
                             }
-                            strcpy( last_slash, rel_url );
+                            strncpy( last_slash, rel_url, strlen ( rel_url ) );
                             if( remove_dots( out_finger,
                                              strlen( out_finger ) ) !=
                                 UPNP_E_SUCCESS ) {

upnp/src/api/upnpapi.c
@@ -1500,6 +1500,7 @@ static int GetDescDocumentAndURL(
 	struct sockaddr_storage serverAddr;
 	int rc = UPNP_E_SUCCESS;
 
+	memset(aliasStr, 0, sizeof(aliasStr));
 	if (description == NULL)
 		return UPNP_E_INVALID_PARAM;
 	/* non-URL description must have configuration specified */
@@ -1551,7 +1552,8 @@ static int GetDescDocumentAndURL(
 	/* Determine alias */
 	if (config_baseURL) {
 		if (descriptionType == UPNPREG_BUF_DESC) {
-			strcpy(aliasStr, "description.xml");
+			strncpy(aliasStr, "description.xml",
+				sizeof(aliasStr) - 1);
 		} else {
 			/* URL or filename */
 			retVal = GetNameForAlias(description, &temp_str);
@@ -1564,7 +1566,7 @@ static int GetDescDocumentAndURL(
 				free(temp_str);
 				return UPNP_E_URL_TOO_BIG;
 			}
-			strcpy(aliasStr, temp_str);
+			strncpy(aliasStr, temp_str, sizeof(aliasStr) - 1);
 		}
 		if (AddressFamily == AF_INET) {
 			get_server_addr((struct sockaddr *)&serverAddr);
@@ -1586,7 +1588,8 @@ static int GetDescDocumentAndURL(
 			ixmlDocument_free(*xmlDoc);
 			return UPNP_E_URL_TOO_BIG;
 		}
-		strcpy(descURL, description);
+		strncpy(descURL, description, strlen(description));
+		descURL[strlen(description)] = '\0';
 	}
 
 	assert(*xmlDoc != NULL);
@@ -1617,7 +1620,8 @@ static int GetDescDocumentAndURL(
 	if (strlen(description) > (LINE_SIZE - 1)) {
 		return UPNP_E_URL_TOO_BIG;
 	}
-	strcpy(descURL, description);
+	strncpy(descURL, description, strlen(description));
+	descURL[strlen(description)] = '\0';
 
 	retVal = UpnpDownloadXmlDoc(description, xmlDoc);
 	if (retVal != UPNP_E_SUCCESS) {
@@ -1986,7 +1990,8 @@ int UpnpSubscribe(
 	HandleUnlock();
 
 	retVal = genaSubscribe(Hnd, EvtUrl, TimeOut, SubsIdTmp);
-	strcpy(SubsId, UpnpString_get_String(SubsIdTmp));
+	memset(SubsId, 0, sizeof(Upnp_SID));
+	strncpy(SubsId, UpnpString_get_String(SubsIdTmp), sizeof(Upnp_SID) - 1);
 
 exit_function:
 	UpnpPrintf(UPNP_ALL, API, __FILE__, __LINE__,
@@ -3436,7 +3441,9 @@ int UpnpGetIfInfo(const char *IfName)
 		    (struct ifreq *)((caddr_t) ifConf.ifc_req + i);
 		i += sizeof *pifReq;
 		/* See if this is the sort of interface we want to deal with. */
-		strcpy(ifReq.ifr_name, pifReq->ifr_name);
+		memset(ifReq.ifr_name, 0, sizeof(ifReq.ifr_name));
+		strncpy(ifReq.ifr_name, pifReq->ifr_name,
+			sizeof(ifReq.ifr_name) - 1);
 		if (ioctl(LocalSock, SIOCGIFFLAGS, &ifReq) < 0) {
 			UpnpPrintf(UPNP_ALL, API, __FILE__, __LINE__,
 				   "Can't get interface flags for %s:\n",
@@ -3868,7 +3875,9 @@ int getlocalhostname(char *out, size_t out_len)
 			(struct ifreq *)((caddr_t)ifConf.ifc_req + i);
 		i += sizeof *pifReq;
 		/* See if this is the sort of interface we want to deal with. */
-		strcpy(ifReq.ifr_name, pifReq->ifr_name);
+		memset(ifReq.ifr_name, 0, sizeof(ifReq.ifr_name));
+		strncpy(ifReq.ifr_name, pifReq->ifr_name,
+			sizeof(ifReq.ifr_name) - 1);
 		if (ioctl(LocalSock, SIOCGIFFLAGS, &ifReq) < 0) {
 			UpnpPrintf(UPNP_ALL, API, __FILE__, __LINE__,
 				"Can't get interface flags for %s:\n",
@@ -3948,6 +3957,7 @@ int UpnpAddVirtualDir(const char *newDirName)
     virtualDirList *pCurVirtualDir;
     char dirName[NAME_SIZE];
 
+    memset( dirName, 0, sizeof( dirName ) );
     if( UpnpSdkInit != 1 ) {
         /* SDK is not initialized */
         return UPNP_E_FINISH;
@@ -3959,9 +3969,9 @@ int UpnpAddVirtualDir(const char *newDirName)
 
     if( *newDirName != '/' ) {
         dirName[0] = '/';
-        strcpy( dirName + 1, newDirName );
+        strncpy( dirName + 1, newDirName, sizeof( dirName ) - 1 );
     } else {
-        strcpy( dirName, newDirName );
+        strncpy( dirName, newDirName, sizeof( dirName ) - 1 );
     }
 
     pCurVirtualDir = pVirtualDirList;
@@ -3980,7 +3990,9 @@ int UpnpAddVirtualDir(const char *newDirName)
         return UPNP_E_OUTOF_MEMORY;
     }
     pNewVirtualDir->next = NULL;
-    strcpy( pNewVirtualDir->dirName, dirName );
+    memset( pNewVirtualDir->dirName, 0, sizeof( pNewVirtualDir->dirName ) );
+    strncpy( pNewVirtualDir->dirName, dirName,
+	sizeof( pNewVirtualDir->dirName ) - 1);
     *( pNewVirtualDir->dirName + strlen( dirName ) ) = 0;
 
     if( pVirtualDirList == NULL ) { /* first virtual dir */

upnp/src/api/upnptools.c
@@ -2,6 +2,7 @@
  *
  * Copyright (c) 2000-2003 Intel Corporation 
  * All rights reserved. 
+ * Copyright (c) 2012 France Telecom All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met: 
@@ -218,13 +219,14 @@ static int addToAction(
 		if (ActBuff == NULL) {
 			return UPNP_E_OUTOF_MEMORY;
 		}
+		memset(ActBuff, 0, HEADER_LENGTH);
 
 		if (response) {
-			sprintf(ActBuff,
+			snprintf(ActBuff, HEADER_LENGTH - 1,
 				"<u:%sResponse xmlns:u=\"%s\">\r\n</u:%sResponse>",
 				ActionName, ServType, ActionName);
 		} else {
-			sprintf(ActBuff,
+			snprintf(ActBuff, HEADER_LENGTH - 1,
 				"<u:%s xmlns:u=\"%s\">\r\n</u:%s>",
 				ActionName, ServType, ActionName);
 		}
@@ -291,13 +293,14 @@ static IXML_Document *makeAction(
 	if (ActBuff == NULL) {
 		return NULL;
 	}
+	memset(ActBuff, 0, HEADER_LENGTH);
 
 	if (response) {
-		sprintf(ActBuff,
+		snprintf(ActBuff, HEADER_LENGTH - 1,
 			"<u:%sResponse xmlns:u=\"%s\">\r\n</u:%sResponse>",
 			ActionName, ServType, ActionName);
 	} else {
-		sprintf(ActBuff,
+		snprintf(ActBuff, HEADER_LENGTH - 1,
 			"<u:%s xmlns:u=\"%s\">\r\n</u:%s>",
 			ActionName, ServType, ActionName);
 	}

upnp/src/gena/gena_ctrlpt.c
@@ -284,6 +284,7 @@ static int gena_subscribe(
 	uri_type dest_url;
 	http_parser_t response;
 
+	memset(timeout_str, 0, sizeof(timeout_str));
 	UpnpString_clear(sid);
 
 	/* request timeout to string */
@@ -291,11 +292,12 @@ static int gena_subscribe(
 		timeout = &local_timeout;
 	}
 	if (*timeout < 0) {
-		strcpy(timeout_str, "infinite");
+		strncpy(timeout_str, "infinite", sizeof(timeout_str) - 1);
 	} else if(*timeout < CP_MINIMUM_SUBSCRIPTION_TIME) {
-		sprintf(timeout_str, "%d", CP_MINIMUM_SUBSCRIPTION_TIME);
+		snprintf(timeout_str, sizeof(timeout_str) - 1,
+			"%d", CP_MINIMUM_SUBSCRIPTION_TIME);
 	} else {
-		sprintf(timeout_str, "%d", *timeout);
+		snprintf(timeout_str, sizeof(timeout_str) - 1, "%d", *timeout);
 	}
 
 	/* parse url */
@@ -513,6 +515,9 @@ int genaSubscribe(
 	UpnpString *EventURL = UpnpString_new();
 	struct Handle_Info *handle_info;
 
+	memset(temp_sid, 0, sizeof(temp_sid));
+	memset(temp_sid2, 0, sizeof(temp_sid2));
+
 	UpnpPrintf(UPNP_INFO, GENA, __FILE__, __LINE__, "GENA SUBSCRIBE BEGIN");
 
 	UpnpString_clear(out_sid);
@@ -545,7 +550,7 @@ int genaSubscribe(
 	/* generate client SID */
 	uuid_create(&uid );
 	uuid_unpack(&uid, temp_sid);
-	sprintf(temp_sid2, "uuid:%s", temp_sid);
+	snprintf(temp_sid2, sizeof(temp_sid2) - 1, "uuid:%s", temp_sid);
 	UpnpString_set_String(out_sid, temp_sid2);
 
 	/* create event url */

upnp/src/gena/gena_device.c
@@ -493,8 +493,10 @@ int genaInitNotify(
 		goto ExitFunction;
 	}
 
-	strcpy(UDN_copy, UDN);
-	strcpy(servId_copy, servId);
+	memset(UDN_copy, 0, strlen(UDN) + 1);
+	strncpy(UDN_copy, UDN, strlen(UDN));
+	memset(servId_copy, 0, strlen(servId) + 1);
+	strncpy(servId_copy, servId, strlen(servId));
 
 	HandleLock();
 
@@ -650,8 +652,10 @@ int genaInitNotifyExt(
 		goto ExitFunction;
 	}
 
-	strcpy(UDN_copy, UDN);
-	strcpy(servId_copy, servId);
+	memset(UDN_copy, 0, strlen(UDN) + 1);
+	strncpy(UDN_copy, UDN, strlen(UDN));
+	memset(servId_copy, 0, strlen(servId) + 1);
+	strncpy(servId_copy, servId, strlen(servId));
 
 	HandleLock();
 
@@ -807,8 +811,10 @@ int genaNotifyAllExt(
 		goto ExitFunction;
 	}
 
-	strcpy(UDN_copy, UDN);
-	strcpy(servId_copy, servId);
+	memset(UDN_copy, 0, strlen(UDN) + 1);
+	strncpy(UDN_copy, UDN, strlen(UDN));
+	memset(servId_copy, 0, strlen(servId) + 1);
+	strncpy(servId_copy, servId, strlen(servId));
 
 	propertySet = ixmlPrintNode((IXML_Node *)PropSet);
 	if (propertySet == NULL) {
@@ -951,8 +957,10 @@ int genaNotifyAll(
 		goto ExitFunction;
 	}
 
-	strcpy(UDN_copy, UDN);
-	strcpy(servId_copy, servId);
+	memset(UDN_copy, 0, strlen(UDN) + 1);
+	strncpy(UDN_copy, UDN, strlen(UDN));
+	memset(servId_copy, 0, strlen(servId) + 1);
+	strncpy(servId_copy, servId, strlen(servId));
 
 	ret = GeneratePropertySet(VarNames, VarValues, var_count, &propertySet);
 	if (ret != XML_SUCCESS) {
@@ -1067,13 +1075,16 @@ static int respond_ok(
     char timeout_str[100];
     int upnp_timeout = UPNP_TIMEOUT;
 
+    memset( timeout_str, 0, sizeof( timeout_str ) );
     http_CalcResponseVersion( request->major_version,
                               request->minor_version, &major, &minor );
 
     if( time_out >= 0 ) {
-        sprintf( timeout_str, "TIMEOUT: Second-%d", time_out );
+        snprintf( timeout_str, sizeof ( timeout_str ) - 1,
+		"TIMEOUT: Second-%d", time_out );
     } else {
-        strcpy( timeout_str, "TIMEOUT: Second-infinite" );
+        strncpy( timeout_str, "TIMEOUT: Second-infinite",
+		sizeof ( timeout_str ) - 1 );
     }
 
     membuffer_init( &response );
@@ -1335,7 +1346,8 @@ void gena_process_subscription_request(
 	/* generate SID */
 	uuid_create(&uid);
 	uuid_unpack(&uid, temp_sid);
-	sprintf(sub->sid, "uuid:%s", temp_sid);
+	memset(sub->sid, 0, sizeof(sub->sid));
+	snprintf(sub->sid, sizeof(sub->sid) - 1, "uuid:%s", temp_sid);
 
 	/* respond OK */
 	if (respond_ok(info, time_out, sub, request) != UPNP_E_SUCCESS) {

upnp/src/genlib/net/http/httpreadwrite.c
@@ -430,6 +430,7 @@ int http_SendMessage(SOCKINFO *info, int *TimeOut, const char *fmt, ...)
 	/* 10 byte allocated for chunk header. */
 	size_t Data_Buf_Size = WEB_SERVER_BUF_SIZE;
 
+	memset(Chunk_Header, 0, sizeof(Chunk_Header));
 	va_start(argp, fmt);
 	while ((c = *fmt++) != 0) {
 		if (c == 'I') {
@@ -507,9 +508,13 @@ int http_SendMessage(SOCKINFO *info, int *TimeOut, const char *fmt, ...)
 					/* Copy CRLF at the end of the chunk */
 					memcpy(file_buf + num_read, "\r\n", 2);
 					/* Hex length for the chunk size. */
-					sprintf(Chunk_Header, "%" PRIzx, num_read);
+					memset(Chunk_Header, 0,
+						sizeof(Chunk_Header));
+					snprintf(Chunk_Header,
+						sizeof(Chunk_Header) - strlen ("\r\n") - 1,
+						"%" PRIzx, num_read);
 					/*itoa(num_read,Chunk_Header,16);  */
-					strcat(Chunk_Header, "\r\n");
+					strncat(Chunk_Header, "\r\n", strlen ("\r\n"));
 					/* Copy the chunk size header  */
 					memcpy(file_buf - strlen(Chunk_Header),
 					       Chunk_Header,
@@ -696,7 +701,8 @@ int http_Download( IN const char *url_str,
 		return ret_code;
 	/* make msg */
 	membuffer_init(&request);
-	strcpy(urlPath, url_str);
+	memset(urlPath, 0, strlen(url_str) + 1);
+	strncpy(urlPath, url_str, strlen(url_str));
 	hoststr = strstr(urlPath, "//");
 	if (hoststr == NULL)
 		return UPNP_E_INVALID_URL;
@@ -1434,6 +1440,7 @@ int http_MakeMessage(membuffer *buf, int http_major_version,
 	const char *month_str = "Jan\0Feb\0Mar\0Apr\0May\0Jun\0"
 	    "Jul\0Aug\0Sep\0Oct\0Nov\0Dec";
 
+	memset(tempbuf, 0, sizeof(tempbuf));
 	va_start(argp, fmt);
 	while ((c = *fmt++) != 0) {
 		if (c == 's') {
@@ -1475,13 +1482,14 @@ int http_MakeMessage(membuffer *buf, int http_major_version,
 		} else if (c == 'd') {
 			/* integer */
 			num = (size_t)va_arg(argp, int);
-			sprintf(tempbuf, "%" PRIzu, num);
+			snprintf(tempbuf, sizeof(tempbuf) - 1, "%" PRIzu, num);
 			if (membuffer_append(buf, tempbuf, strlen(tempbuf)))
 				goto error_handler;
 		} else if (c == 'h') {
 			/* off_t */
 			bignum = (off_t) va_arg(argp, off_t);
-			sprintf(tempbuf, "%" PRId64, (int64_t) bignum);
+			snprintf(tempbuf, sizeof(tempbuf) - 1, "%" PRId64,
+				(int64_t) bignum);
 			if (membuffer_append(buf, tempbuf, strlen(tempbuf)))
 				goto error_handler;
 		} else if (c == 't' || c == 'D') {
@@ -1499,7 +1507,7 @@ int http_MakeMessage(membuffer *buf, int http_major_version,
 			}
 			assert(loc_time);
 			date = gmtime(loc_time);
-			sprintf(tempbuf,
+			snprintf(tempbuf, sizeof(tempbuf) - 1,
 				"%s%s, %02d %s %d %02d:%02d:%02d GMT%s",
 				start_str, &weekday_str[date->tm_wday * 4],
 				date->tm_mday, &month_str[date->tm_mon * 4],
@@ -1556,7 +1564,7 @@ int http_MakeMessage(membuffer *buf, int http_major_version,
 			/*   e.g.: 'HTTP/1.1 200 OK' code */
 			status_code = (int)va_arg(argp, int);
 			assert(status_code > 0);
-			sprintf(tempbuf, "HTTP/%d.%d %d ",
+			snprintf(tempbuf, sizeof(tempbuf) - 1, "HTTP/%d.%d %d ",
 				http_major_version, http_minor_version,
 				status_code);
 			/* str */
@@ -1567,7 +1575,7 @@ int http_MakeMessage(membuffer *buf, int http_major_version,
 		} else if (c == 'B') {
 			/* body of a simple reply */
 			status_code = (int)va_arg(argp, int);
-			sprintf(tempbuf, "%s%d %s%s",
+			snprintf(tempbuf, sizeof(tempbuf) - 1, "%s%d %s%s",
 				"<html><body><h1>",
 				status_code, http_get_code_text(status_code),
 				"</h1></body></html>");
@@ -1702,7 +1710,7 @@ int MakeGetMessageEx( const char *url_str,
 			break;
 		}
 		memset(urlPath, 0, strlen(url_str) + 1);
-		strcpy(urlPath, url_str);
+		strncpy(urlPath, url_str, strlen(url_str));
 		hoststr = strstr(urlPath, "//");
 		if (hoststr == NULL) {
 			errCode = UPNP_E_INVALID_URL;
@@ -1806,7 +1814,8 @@ int http_OpenHttpGetEx(
 			break;
 		}
 		memset(&rangeBuf, 0, sizeof(rangeBuf));
-		sprintf(rangeBuf.RangeHeader,
+		snprintf(rangeBuf.RangeHeader,
+			sizeof(rangeBuf.RangeHeader) - 1,
 			"Range: bytes=%d-%d\r\n", lowRange, highRange);
 		membuffer_init(&request);
 		errCode = MakeGetMessageEx(url_str, &request, &url, &rangeBuf);

upnp/src/genlib/net/http/webserver.c
@@ -318,6 +318,7 @@ static UPNP_INLINE int get_content_type(
 	temp = malloc(length);
 	if (!temp)
 		return UPNP_E_OUTOF_MEMORY;
+	memset(temp, 0, length);
 	sprintf(temp, "%s/%s", type, subtype);
 	UpnpFileInfo_set_ContentType(fileInfo, temp);
 	free(temp);
@@ -773,7 +774,8 @@ static int CreateHTTPRangeResponseHeader(
 	RangeInput = malloc(strlen(ByteRangeSpecifier) + 1);
 	if (!RangeInput)
 		return UPNP_E_OUTOF_MEMORY;
-	strcpy(RangeInput, ByteRangeSpecifier);
+	memset(RangeInput, 0, strlen(ByteRangeSpecifier) + 1);
+	strncpy(RangeInput, ByteRangeSpecifier, strlen(ByteRangeSpecifier));
 	/* CONTENT-RANGE: bytes 222-3333/4000  HTTP_PARTIAL_CONTENT */
 	if (StrStr(RangeInput, "bytes") == NULL ||
 	    (Ptr = StrStr(RangeInput, "=")) == NULL) {
@@ -792,13 +794,15 @@ static int CreateHTTPRangeResponseHeader(
 			free(RangeInput);
 			return HTTP_REQUEST_RANGE_NOT_SATISFIABLE;
 		}
+		memset(Instr->RangeHeader, 0, sizeof(Instr->RangeHeader));
 		if (FirstByte >= 0 && LastByte >= 0 && LastByte >= FirstByte) {
 			if (LastByte >= FileLength)
 				LastByte = FileLength - 1;
 			Instr->RangeOffset = FirstByte;
 			Instr->ReadSendSize = LastByte - FirstByte + 1;
 			/* Data between two range. */
-			sprintf(Instr->RangeHeader,
+			snprintf(Instr->RangeHeader,
+				sizeof(Instr->RangeHeader) - 1,
 				"CONTENT-RANGE: bytes %" PRId64
 				"-%" PRId64 "/%" PRId64 "\r\n",
 				(int64_t)FirstByte,
@@ -808,7 +812,10 @@ static int CreateHTTPRangeResponseHeader(
 			   && FirstByte < FileLength) {
 			Instr->RangeOffset = FirstByte;
 			Instr->ReadSendSize = FileLength - FirstByte;
-			sprintf(Instr->RangeHeader,
+			memset(Instr->RangeHeader, 0,
+				sizeof(Instr->RangeHeader));
+			snprintf(Instr->RangeHeader,
+				sizeof(Instr->RangeHeader) - 1,
 				"CONTENT-RANGE: bytes %" PRId64
 				"-%" PRId64 "/%" PRId64 "\r\n",
 				(int64_t)FirstByte,
@@ -818,7 +825,8 @@ static int CreateHTTPRangeResponseHeader(
 			if (LastByte >= FileLength) {
 				Instr->RangeOffset = 0;
 				Instr->ReadSendSize = FileLength;
-				sprintf(Instr->RangeHeader,
+				snprintf(Instr->RangeHeader,
+					sizeof(Instr->RangeHeader) - 1,
 					"CONTENT-RANGE: bytes 0-%" PRId64
 					"/%" PRId64 "\r\n",
 					(int64_t)(FileLength - 1),
@@ -826,7 +834,8 @@ static int CreateHTTPRangeResponseHeader(
 			} else {
 				Instr->RangeOffset = FileLength - LastByte;
 				Instr->ReadSendSize = LastByte;
-				sprintf(Instr->RangeHeader,
+				snprintf(Instr->RangeHeader,
+					sizeof(Instr->RangeHeader) - 1,
 					"CONTENT-RANGE: bytes %" PRId64
 					"-%" PRId64 "/%" PRId64 "\r\n",
 					(int64_t)(FileLength - LastByte + 1),

upnp/src/soap/soap_device.c
@@ -194,7 +194,8 @@ static void send_error_response(
 	char err_code_str[30];
 	membuffer headers;
 
-	sprintf(err_code_str, "%d", error_code);
+	memset(err_code_str, 0, sizeof(err_code_str));
+	snprintf(err_code_str, sizeof(err_code_str) - 1, "%d", error_code);
 	/* calc body len */
 	content_length = (off_t) (strlen(start_body) + strlen(err_code_str) +
 				  strlen(mid_body) + strlen(err_msg) +

commit 0508fb0d6e55a9066f3722843aa782632d302276

Fix resolve_rel_url()
This reworks commit 0edaf3361db01425cae0daee7dc3f6039f381a17, which
broke resolving relative url, where the relative URL is shorter than the
absolute URL:
    "http://127.0.0.1:6544/getDeviceDesc" + "CDS_Event"
    Wrong: "http://127.0.0.1:6544/CDS_EventDesc"
    Right: "http://127.0.0.1:6544/CDS_Event"

While reviewing that commit, improve code by:
1. Move the simple cases to the beginning of the function.
2. Keep track of the remaining target buffer size.
3. Fix URI concatenation with queries.
4. Fix URI concatenation with fragments.

Signed-off-by: Marcelo Roberto Jimenez <mroberto@users.sourceforge.net>

upnp/src/genlib/net/uri/uri.c
@@ -580,115 +580,113 @@ char *resolve_rel_url(char *base_url, char *rel_url)
 {
     uri_type base;
     uri_type rel;
+    int rv;
 
-    size_t i = (size_t)0;
-    char *finger = NULL;
-
-    char *last_slash = NULL;
-
-    char *out = NULL;
-
-    if( base_url && rel_url ) {
-        out =
-            ( char * )malloc( strlen( base_url ) + strlen( rel_url ) + (size_t)2 );
-    } else {
-        if( rel_url )
-            return strdup( rel_url );
-        else
+    if (!base_url) {
+        if (!rel_url)
             return NULL;
+        return strdup(rel_url);
     }
 
-    if( out == NULL ) {
+    size_t len_rel = strlen(rel_url);
+    if (parse_uri(rel_url, len_rel, &rel) != HTTP_SUCCESS)
         return NULL;
+    if (rel.type == (enum uriType)ABSOLUTE)
+        return strdup(rel_url);
+
+    size_t len_base = strlen(base_url);
+    if ((parse_uri(base_url, len_base, &base) != HTTP_SUCCESS)
+            || (base.type != (enum uriType)ABSOLUTE))
+        return NULL;
+    if (len_rel == (size_t)0)
+        return strdup(base_url);
+
+    size_t len = len_base + len_rel + (size_t)2;
+    char *out = (char *)malloc(len);
+    if (out == NULL)
+        return NULL;
+    memset(out, 0, len);
+    char *out_finger = out;
+
+    /* scheme */
+    rv = snprintf(out_finger, len, "%.*s:", (int)base.scheme.size, base.scheme.buff);
+    if (rv < 0 || rv >= len)
+        goto error;
+    out_finger += rv;
+    len -= rv;
+
+    /* authority */
+    if (rel.hostport.text.size > (size_t)0) {
+        rv = snprintf(out_finger, len, "%s", rel_url);
+        if (rv < 0 || rv >= len)
+            goto error;
+        return out;
     }
-    memset( out, 0, strlen( base_url ) + strlen( rel_url ) + (size_t)2 );
+    if (base.hostport.text.size > (size_t)0) {
+	rv = snprintf(out_finger, len, "//%.*s", (int)base.hostport.text.size, base.hostport.text.buff);
+	if (rv < 0 || rv >= len)
+	    goto error;
+	out_finger += rv;
+	len -= rv;
+    }
 
-    if( ( parse_uri( rel_url, strlen( rel_url ), &rel ) ) == HTTP_SUCCESS ) {
-
-        if( rel.type == ( enum uriType) ABSOLUTE ) {
-
-            strncpy( out, rel_url, strlen ( rel_url ) );
-        } else {
-
-            if( ( parse_uri( base_url, strlen( base_url ), &base ) ==
-                  HTTP_SUCCESS )
-                && ( base.type == ( enum uriType ) ABSOLUTE ) ) {
-
-                if( strlen( rel_url ) == (size_t)0 ) {
-                    strncpy( out, base_url, strlen ( base_url ) );
-                } else {
-                    char *out_finger = out;
-                    assert( base.scheme.size + (size_t)1 /* ':' */ <= strlen ( base_url ) );
-                    memcpy( out, base.scheme.buff, base.scheme.size );
-                    out_finger += base.scheme.size;
-                    ( *out_finger ) = ':';
-                    out_finger++;
-
-                    if( rel.hostport.text.size > (size_t)0 ) {
-                        snprintf( out_finger, strlen( rel_url ) + (size_t)1,
-                                  "%s", rel_url );
-                    } else {
-                        if( base.hostport.text.size > (size_t)0 ) {
-                            assert( base.scheme.size + (size_t)1
-                                + base.hostport.text.size + (size_t)2 /* "//" */ <= strlen ( base_url ) );
-                            memcpy( out_finger, "//", (size_t)2 );
-                            out_finger += 2;
-                            memcpy( out_finger, base.hostport.text.buff,
-                                    base.hostport.text.size );
-                            out_finger += base.hostport.text.size;
-                        }
-
-                        if( rel.path_type == ( enum pathType ) ABS_PATH ) {
-                            strncpy( out_finger, rel_url, strlen ( rel_url ) );
-
-                        } else {
-                            char temp_path = '/';
-
-                            if( base.pathquery.size == (size_t)0 ) {
-                                base.pathquery.size = (size_t)1;
-                                base.pathquery.buff = &temp_path;
-                            }
-
-                            assert( base.scheme.size + (size_t)1 + base.hostport.text.size + (size_t)2
-                                + base.pathquery.size <= strlen ( base_url ) + (size_t)1 /* temp_path */);
-                            finger = out_finger;
-                            last_slash = finger;
-                            i = (size_t)0;
-                            while( ( i < base.pathquery.size ) &&
-                                   ( base.pathquery.buff[i] != '?' ) ) {
-                                ( *finger ) = base.pathquery.buff[i];
-                                if( base.pathquery.buff[i] == '/' )
-                                    last_slash = finger + 1;
-                                i++;
-                                finger++;
-
-                            }
-                            strncpy( last_slash, rel_url, strlen ( rel_url ) );
-                            if( remove_dots( out_finger,
-                                             strlen( out_finger ) ) !=
-                                UPNP_E_SUCCESS ) {
-                                free(out);
-                                /* free(rel_url); */
-                                return NULL;
-                            }
-                        }
-
-                    }
-                }
-            } else {
-                free(out);
-                /* free(rel_url); */
-                return NULL;
-            }
-        }
+    /* path */
+    char *path = out_finger;
+    if (rel.path_type == (enum pathType)ABS_PATH) {
+	rv = snprintf(out_finger, len, "%s", rel_url);
+    } else if (base.pathquery.size == (size_t)0) {
+	rv = snprintf(out_finger, len, "/%s", rel_url);
     } else {
-        free(out);
-        /* free(rel_url); */          
-        return NULL;
-    }
+	if (rel.pathquery.size == (size_t)0) {
+	    rv = snprintf(out_finger, len, "%.*s", (int)base.pathquery.size, base.pathquery.buff);
+	} else {
+	    if (len < base.pathquery.size)
+		goto error;
+	    size_t i = (size_t)0, prefix = (size_t)1;
+	    while (i < base.pathquery.size) {
+		out_finger[i] = base.pathquery.buff[i];
+		switch (base.pathquery.buff[i++]) {
+		    case '/':
+			prefix = i;
+			/* fall-through */
+		    default:
+			continue;
+		    case '?': /* query */
+			if (rel.pathquery.buff[0] == '?')
+			    prefix = --i;
+		}
+		break;
+	    }
+	    out_finger += prefix;
+	    len -= prefix;
+	    rv = snprintf(out_finger, len, "%.*s", (int)rel.pathquery.size, rel.pathquery.buff);
+	}
+	if (rv < 0 || rv >= len)
+	    goto error;
+	out_finger += rv;
+	len -= rv;
 
-    /* free(rel_url); */
+	/* fragment */
+	if (rel.fragment.size > (size_t)0)
+	    rv = snprintf(out_finger, len, "#%.*s", (int)rel.fragment.size, rel.fragment.buff);
+	else if (base.fragment.size > (size_t)0)
+	    rv = snprintf(out_finger, len, "#%.*s", (int)base.fragment.size, base.fragment.buff);
+	else
+	    rv = 0;
+    }
+    if (rv < 0 || rv >= len)
+	goto error;
+    out_finger += rv;
+    len -= rv;
+
+    if (remove_dots(path, out_finger - path) != UPNP_E_SUCCESS)
+	goto error;
+
     return out;
+
+error:
+    free(out);
+    return NULL;
 }
 
 