83f3b34552f6cb0c968e85c22e7b0f9a297cb7e0
Nick Leverton <nick@leverton.org>
2016-01-07 20:37:32 -0200

    SF Patches #60, Creator: Jean-Francois Dockes
    When libupnp is configured with --enable-ipv6 but ipv6 is not available
    on the system (for example because the ipv6 code is not loaded in a Linux
    kernel as is the case by default on Raspbian), the ipv6 socket creation
    call will fail in miniserver.c and the library init will fail, even if
    the ipv4 initialisation would have succeeded.
    
    Let a library configured with --enable-ipv6 initialize in ipv4-only
    mode if ipv6 is not available instead of failing. This can
    happen if no ipv6 code is configured or loaded in the kernel.
    
    Don't fail if IPv6 is unavailable.
    We might be an IPv6 enabled distro build running on an IPv4-only custom kernel.
    
    Signed-off-by: Marcelo Roberto Jimenez <mroberto@users.sourceforge.net>

--- upnp/src/genlib/miniserver/miniserver.c
+++ upnp/src/genlib/miniserver/miniserver.c
@@ -521,7 +521,6 @@ static int get_miniserver_sockets(
 	struct sockaddr_in6* serverAddr6 = (struct sockaddr_in6*)&__ss_v6;
 	SOCKET listenfd6;
 	uint16_t actual_port6 = 0u;
-	int onOff;
 #endif
 	int ret_code;
 	int reuseaddr_on = 0;
@@ -532,23 +531,41 @@ static int get_miniserver_sockets(
 	 * that we don't have an IPv4/IPv6 stack. */
 	listenfd4 = socket(AF_INET, SOCK_STREAM, 0);
 	if (listenfd4 == INVALID_SOCKET) {
-		return UPNP_E_OUTOF_SOCKET;
+		strerror_r(errno, errorBuffer, ERROR_BUFFER_LEN);
+		UpnpPrintf(UPNP_INFO, MSERV, __FILE__, __LINE__,
+			"get_miniserver_sockets: IPv4 socket not available: %s\n",
+			errorBuffer);
 	}
 #ifdef UPNP_ENABLE_IPV6
 	listenfd6 = socket(AF_INET6, SOCK_STREAM, 0);
 	if (listenfd6 == INVALID_SOCKET) {
-		sock_close(listenfd4);
-		return UPNP_E_OUTOF_SOCKET;
-	}
-	onOff = 1;
-	sockError = setsockopt(listenfd6, IPPROTO_IPV6, IPV6_V6ONLY,
-			 (char *)&onOff, sizeof(onOff));
-	if (sockError == SOCKET_ERROR) {
-		sock_close(listenfd4);
-		sock_close(listenfd6);
-		return UPNP_E_SOCKET_BIND;
+		strerror_r(errno, errorBuffer, ERROR_BUFFER_LEN);
+		UpnpPrintf(UPNP_INFO, MSERV, __FILE__, __LINE__,
+			"get_miniserver_sockets: IPv6 socket not available: %s\n",
+			errorBuffer);
+	} else {
+		int onOff = 1;
+		sockError = setsockopt(listenfd6, IPPROTO_IPV6, IPV6_V6ONLY,
+							   (char *)&onOff, sizeof(onOff));
+		if (sockError == SOCKET_ERROR) {
+			strerror_r(errno, errorBuffer, ERROR_BUFFER_LEN);
+			UpnpPrintf(UPNP_INFO, MSERV, __FILE__, __LINE__,
+				"get_miniserver_sockets: unable to set IPv6 socket protocol: %s\n",
+				errorBuffer);
+			sock_close(listenfd6);
+			listenfd6 = INVALID_SOCKET;
+		}
 	}
 #endif
+	if (listenfd4 == INVALID_SOCKET
+#ifdef UPNP_ENABLE_IPV6
+	    && listenfd6 == INVALID_SOCKET
+#endif
+	) {
+		UpnpPrintf(UPNP_CRITICAL, MSERV, __FILE__, __LINE__,
+			"get_miniserver_sockets: no protocols available\n");
+		return UPNP_E_OUTOF_SOCKET;
+	}
 	/* As per the IANA specifications for the use of ports by applications
 	 * override the listen port passed in with the first available. */
 	if (listen_port4 < APPLICATION_LISTENING_PORT) {
