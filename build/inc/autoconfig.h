7ea7bb66ed0419f009eb768a2cda4b239e832c81
Fabrice Fontaine <fabrice.fontaine@orange.com>
2016-12-20 09:27:44 +0100

    Homekeeping for the next release.
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

--- build/inc/autoconfig.h
+++ build/inc/autoconfig.h
@@ -108,7 +108,7 @@
 #define PACKAGE_NAME "libupnp"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libupnp 1.6.21"
+#define PACKAGE_STRING "libupnp 1.6.22"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "libupnp"
@@ -117,7 +117,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.6.21"
+#define PACKAGE_VERSION "1.6.22"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
@@ -178,13 +178,13 @@
 #define UPNP_VERSION_MINOR 6
 
 /* see upnpconfig.h */
-#define UPNP_VERSION_PATCH 21
+#define UPNP_VERSION_PATCH 22
 
 /* see upnpconfig.h */
-#define UPNP_VERSION_STRING "1.6.21"
+#define UPNP_VERSION_STRING "1.6.22"
 
 /* Version number of package */
-#define VERSION "1.6.21"
+#define VERSION "1.6.22"
 
 /* File Offset size */
 #define _FILE_OFFSET_BITS 64
--- libupnp.spec
+++ libupnp.spec
@@ -1,4 +1,4 @@
-Version: 1.6.21
+Version: 1.6.22
 Summary: Universal Plug and Play (UPnP) SDK
 Name: libupnp
 Release: 1%{?dist}
