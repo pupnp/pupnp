a5d811a59ff7a1a6b1fe6e09ee9018cfccd723aa
Marcelo Jimenez <mroberto@users.sourceforge.net>
2017-01-20 21:20:31 -0200

    Fix for unit tests
    Signed-off-by: Marcelo Roberto Jimenez <mroberto@users.sourceforge.net>

--- ixml/test/test_document.c
+++ ixml/test/test_document.c
@@ -1,34 +1,34 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
-///////////////////////////////////////////////////////////////////////////
-//
-// Copyright (c) 2006 R?mi Turboult <r3mi@users.sourceforge.net>
-// All rights reserved. 
-//
-// Redistribution and use in source and binary forms, with or without 
-// modification, are permitted provided that the following conditions are met: 
-//
-// * Redistributions of source code must retain the above copyright notice, 
-// this list of conditions and the following disclaimer. 
-// * Redistributions in binary form must reproduce the above copyright notice, 
-// this list of conditions and the following disclaimer in the documentation 
-// and/or other materials provided with the distribution. 
-// * Neither name of Intel Corporation nor the names of its contributors 
-// may be used to endorse or promote products derived from this software 
-// without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL INTEL OR 
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
-// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-///////////////////////////////////////////////////////////////////////////
+/**************************************************************************
+ *
+ * Copyright (c) 2006 R?mi Turboult <r3mi@users.sourceforge.net>
+ * All rights reserved. 
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met: 
+ *
+ * * Redistributions of source code must retain the above copyright notice, 
+ * this list of conditions and the following disclaimer. 
+ * * Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation 
+ * and/or other materials provided with the distribution. 
+ * * Neither name of Intel Corporation nor the names of its contributors 
+ * may be used to endorse or promote products derived from this software 
+ * without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL INTEL OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *************************************************************************/
 
 
 #include "ixml.h"
@@ -82,7 +82,7 @@ main (int argc, char* argv[])
 	if (argc < 2) {
 		fprintf (stderr, "Usage: %s [xml files to load]\n",
 			 argv[0]);
-		exit (EXIT_FAILURE); // ---------->
+		exit (EXIT_FAILURE);
 	}
 
 	for (i = 1; i < argc; i++) {
@@ -101,7 +101,7 @@ main (int argc, char* argv[])
 				 "** error : can't load document %s : "
 				 "error %d (%s)\n",
 				 argv[i], rc, get_ixml_error_string (rc));
-			exit (EXIT_FAILURE); // ---------->
+			exit (EXIT_FAILURE);
 		}
 
 		printf ("OK\n");
@@ -114,7 +114,7 @@ main (int argc, char* argv[])
 			fprintf (stderr, 
 				 "** error : can't print loaded document %s\n",
 				 argv[i]);
-			exit (EXIT_FAILURE); // ---------->
+			exit (EXIT_FAILURE);
 		}
 		p = s + strlen(s)-1;
 		while (isspace(*p) && p > s)
@@ -123,7 +123,7 @@ main (int argc, char* argv[])
 			fprintf (stderr, 
 				 "** error : malformed printed document '%s' :"
 				 "%s\n", argv[i], s);
-			exit (EXIT_FAILURE); // ---------->
+			exit (EXIT_FAILURE);
 		}
 
 		printf ("OK\n");
@@ -134,7 +134,4 @@ main (int argc, char* argv[])
 	
 	exit (EXIT_SUCCESS);
 }
-
-
-
 
