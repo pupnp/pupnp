pupnp/pupnp

commit 9c2e8ec8a0291ebe81959009e2f78edbdb47ced5

extra headers
Allow extra headers to be sent to client in the File_Info struct by
adding extra_headers, an array of struct Extra_Headers.

It contains "name" and "value" and the callee can populate a DOMString
"resp" to add a header as well in the reponse

upnp/src/genlib/net/http/httpreadwrite.c
@@ -1658,6 +1658,20 @@ int http_MakeMessage(membuffer *buf, int http_major_version,
 	memset(tempbuf, 0, sizeof(tempbuf));
 	va_start(argp, fmt);
 	while ((c = *fmt++)) {
+		if (c == 'E') {
+			struct Extra_Headers *extras;
+			/* array of extra headers */
+			extras = (struct Extra_Headers *) va_arg(argp, struct Extra_Headers *);
+			while (extras->name) {
+				if (extras->resp) {
+					if (membuffer_append(buf, extras->resp, strlen(extras->resp)))
+						goto error_handler;
+					if (membuffer_append(buf, "\r\n", (size_t)2))
+						goto error_handler;
+				}
+				extras++;
+			}
+		}
 		if (c == 's') {
 			/* C string */
 			s = (char *)va_arg(argp, char *);

upnp/inc/upnp.h
@@ -799,6 +799,16 @@ struct Upnp_Subscription_Request
 
 };
 
+struct Extra_Headers
+{
+	/** The length of the file. A length less than 0 indicates the size
+	*  is unknown, and data will be sent until 0 bytes are returned from
+	*  a read call. */
+	char *name;
+	char *value;
+	DOMString resp;
+};
+
 struct File_Info
 {
 	/** The length of the file. A length less than 0 indicates the size 
@@ -822,6 +832,11 @@ struct File_Info
 	*  by the caller using {\bf ixmlCloneDOMString}.  When finished 
 	*  with it, the SDK frees the {\bf DOMString}. */
 	DOMString content_type;
+
+	/** Headers to be modified / added. A modified response must be allocated
+	* by the caller using {\bf ixmlCloneDOMString}.  When finished with it,
+	* the SDK frees all of them. */
+	struct Extra_Headers *extra_headers;
 };
 #endif /* UPNP_VERSION < 10800 */
 

upnp/src/genlib/net/http/webserver.c
@@ -1009,6 +1009,81 @@ static int CheckOtherHTTPHeaders(
 }
 
 /*!
+ * \brief Build an array of unrecognized headers.
+ *
+ * \return nothing
+ */
+#define MAX_EXTRA_HEADERS 128
+static int ExtraHTTPHeaders(
+	/*! [in] HTTP Request message. */
+	http_message_t *Req,
+	struct Extra_Headers **ExtraHeaders)
+{
+	http_header_t *header;
+	ListNode *node;
+	int index, nb_extra = 0;
+	struct Extra_Headers *extra_headers;
+
+	node = ListHead(&Req->headers);
+	extra_headers = *ExtraHeaders =
+		(struct Extra_Headers*) malloc(MAX_EXTRA_HEADERS * sizeof(struct Extra_Headers));
+	if (!extra_headers) {
+		return HTTP_INTERNAL_SERVER_ERROR;
+	}
+	while (node != NULL) {
+		header = (http_header_t *) node->item;
+		/* find header type. */
+		index = map_str_to_int((const char *)header->name.buf,
+				header->name.length, Http_Header_Names,
+				NUM_HTTP_HEADER_NAMES, FALSE);
+		if (index < 0) {
+			extra_headers->name = (char *)malloc(header->name.length + 1);
+			extra_headers->value = (char *)malloc(header->value.length + 1);
+			if (!extra_headers->name || !extra_headers->value) {
+				/* cleanup will be made by caller */
+				return HTTP_INTERNAL_SERVER_ERROR;
+			}
+			memcpy(extra_headers->name, header->name.buf, header->name.length);
+			memcpy(extra_headers->value, header->value.buf, header->value.length);
+			extra_headers->name[header->name.length] = '\0';
+			extra_headers->value[header->value.length] = '\0';
+			extra_headers->resp = NULL;
+
+			extra_headers++;
+			nb_extra++;
+
+			if (nb_extra == MAX_EXTRA_HEADERS - 1) {
+				break;
+			}
+		}
+		node = ListNext(&Req->headers, node);
+	}
+	extra_headers->name = extra_headers->value = extra_headers->resp = NULL;
+	return HTTP_OK;
+}
+
+static void FreeExtraHTTPHeaders(
+	/*! [in] extra HTTP headers to free. */
+	struct Extra_Headers *ExtraHeaders)
+{
+	struct Extra_Headers *extra_headers = ExtraHeaders;
+
+	if (!ExtraHeaders) {
+		return;
+	}
+
+	while (extra_headers->name) {
+		free(extra_headers->name);
+		if (extra_headers->value) free(extra_headers->value);
+		if (extra_headers->resp) ixmlFreeDOMString(extra_headers->resp);
+		extra_headers++;
+	}
+
+	free(ExtraHeaders);
+}
+
+
+/*!
  * \brief Processes the request and returns the result in the output parameters.
  *
  * \return
@@ -1047,7 +1122,7 @@ static int process_request(
 	int resp_minor;
 	int alias_grabbed;
 	size_t dummy;
-	const char *extra_headers = NULL;
+	struct Extra_Headers *extra_headers = NULL;
 
 	print_http_headers(req);
 	url = &req->uri;
@@ -1111,7 +1186,12 @@ static int process_request(
 	}
 	if (using_virtual_dir) {
 		if (req->method != HTTPMETHOD_POST) {
+			if ((code = ExtraHTTPHeaders(req, &extra_headers)) != HTTP_OK) {
+				err_code = code;
+				goto error_handler;
+			}
 			/* get file info */
+			finfo.extra_headers = extra_headers;
 			if (virtualDirCallback.
 			    get_info(filename->buf, &finfo) != 0) {
 				err_code = HTTP_NOT_FOUND;
@@ -1205,19 +1285,16 @@ static int process_request(
 	RespInstr->ReadSendSize = finfo.file_length;
 	/* Check other header field. */
 	if ((code =
-	     CheckOtherHTTPHeaders(req, RespInstr,
+		 CheckOtherHTTPHeaders(req, RespInstr,
 				   finfo.file_length)) != HTTP_OK) {
 		err_code = code;
 		goto error_handler;
 	}
+
 	if (req->method == HTTPMETHOD_POST) {
 		*rtype = RESP_POST;
 		err_code = HTTP_OK;
 		goto error_handler;
-	}
-	/*extra_headers = UpnpFileInfo_get_ExtraHeaders(finfo); */
-	if (!extra_headers) {
-		extra_headers = "";
 	}
 
 	/* Check if chunked encoding should be used. */
@@ -1238,7 +1315,7 @@ static int process_request(
 		/* Content-Range: bytes 222-3333/4000  HTTP_PARTIAL_CONTENT */
 		/* Transfer-Encoding: chunked */
 		if (http_MakeMessage(headers, resp_major, resp_minor,
-		    "R" "T" "GKLD" "s" "tcS" "Xc" "sCc",
+		    "R" "T" "GKLD" "s" "tcS" "Xc" "ECc",
 		    HTTP_PARTIAL_CONTENT,	/* status code */
 		    finfo.content_type,	/* content type */
 		    RespInstr,	/* range info */
@@ -1251,54 +1328,54 @@ static int process_request(
 	} else if (RespInstr->IsRangeActive && !RespInstr->IsChunkActive) {
 		/* Content-Range: bytes 222-3333/4000  HTTP_PARTIAL_CONTENT */
 		if (http_MakeMessage(headers, resp_major, resp_minor,
-		    "R" "N" "T" "GLD" "s" "tcS" "Xc" "sCc",
-		    HTTP_PARTIAL_CONTENT,	/* status code */
-		    RespInstr->ReadSendSize,	/* content length */
-		    finfo.content_type,	/* content type */
-		    RespInstr,	/* range info */
-		    RespInstr,	/* language info */
-		    "LAST-MODIFIED: ",
-		    &finfo.last_modified,
-		    X_USER_AGENT, extra_headers) != 0) {
+			"R" "N" "T" "GLD" "s" "tcS" "Xc" "ECc",
+			HTTP_PARTIAL_CONTENT,	/* status code */
+			RespInstr->ReadSendSize,	/* content length */
+			finfo.content_type,	/* content type */
+			RespInstr,	/* range info */
+			RespInstr,	/* language info */
+			"LAST-MODIFIED: ",
+			&finfo.last_modified,
+			X_USER_AGENT, extra_headers) != 0) {
 			goto error_handler;
 		}
 	} else if (!RespInstr->IsRangeActive && RespInstr->IsChunkActive) {
 		/* Transfer-Encoding: chunked */
 		if (http_MakeMessage(headers, resp_major, resp_minor,
-		    "RK" "TLD" "s" "tcS" "Xc" "sCc",
-		    HTTP_OK,	/* status code */
-		    finfo.content_type,	/* content type */
-		    RespInstr,	/* language info */
-		    "LAST-MODIFIED: ",
-		    &finfo.last_modified,
-		    X_USER_AGENT, extra_headers) != 0) {
+			"RK" "TLD" "s" "tcS" "Xc" "ECc",
+			HTTP_OK,	/* status code */
+			finfo.content_type,	/* content type */
+			RespInstr,	/* language info */
+			"LAST-MODIFIED: ",
+			&finfo.last_modified,
+			X_USER_AGENT, extra_headers) != 0) {
 			goto error_handler;
 		}
 	} else {
 		/* !RespInstr->IsRangeActive && !RespInstr->IsChunkActive */
 		if (RespInstr->ReadSendSize >= 0) {
 			if (http_MakeMessage(headers, resp_major, resp_minor,
-			    "R" "N" "TLD" "s" "tcS" "Xc" "sCc",
-			    HTTP_OK,	/* status code */
-			    RespInstr->ReadSendSize,	/* content length */
-			    finfo.content_type,	/* content type */
-			    RespInstr,	/* language info */
-			    "LAST-MODIFIED: ",
-			    &finfo.last_modified,
-			    X_USER_AGENT,
-			    extra_headers) != 0) {
+				"R" "N" "TLD" "s" "tcS" "Xc" "ECc",
+				HTTP_OK,	/* status code */
+				RespInstr->ReadSendSize,	/* content length */
+				finfo.content_type,	/* content type */
+				RespInstr,	/* language info */
+				"LAST-MODIFIED: ",
+				&finfo.last_modified,
+				X_USER_AGENT,
+				extra_headers) != 0) {
 				goto error_handler;
 			}
 		} else {
 			if (http_MakeMessage(headers, resp_major, resp_minor,
-			    "R" "TLD" "s" "tcS" "Xc" "sCc",
-			    HTTP_OK,	/* status code */
-			    finfo.content_type,	/* content type */
-			    RespInstr,	/* language info */
-			    "LAST-MODIFIED: ",
-			    &finfo.last_modified,
-			    X_USER_AGENT,
-			    extra_headers) != 0) {
+				"R" "TLD" "s" "tcS" "Xc" "ECc",
+				HTTP_OK,	/* status code */
+				finfo.content_type,	/* content type */
+				RespInstr,	/* language info */
+				"LAST-MODIFIED: ",
+				&finfo.last_modified,
+				X_USER_AGENT,
+				extra_headers) != 0) {
 				goto error_handler;
 			}
 		}
@@ -1323,6 +1400,7 @@ static int process_request(
 
  error_handler:
 	free(request_doc);
+	FreeExtraHTTPHeaders(extra_headers);
 	ixmlFreeDOMString(finfo.content_type);
 	if (err_code != HTTP_OK && alias_grabbed) {
 		alias_release(alias);

commit 7af4e2ed0165c43bfa4bf9b3ffaa1ea925602662

Fix assertion error when http_MakeMessage is called with E in fmt
An assert(0) is hit if fmt contains a char that is not expected. Support
for 'E' was added without making sure that this assert isn't reached.
This only hurts in debug builds and so shows that nobody tests these :-)

Fixes: 9c2e8ec8a029 ("extra headers")
Reported-by: Tobias Frost <tobi@coldtobi.de>

upnp/src/genlib/net/http/httpreadwrite.c
@@ -1673,8 +1673,7 @@ int http_MakeMessage(membuffer *buf, int http_major_version,
 					extras++;
 				}
 			}
-		}
-		if (c == 's') {
+		} else if (c == 's') {
 			/* C string */
 			s = (char *)va_arg(argp, char *);
 			assert(s);