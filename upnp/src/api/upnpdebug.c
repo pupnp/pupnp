pupnp/pupnp

commit 0df404f9d64439e853ddc25e1a7d85f8fcc91eed

Fix crash when debug is enabled
- If the error or info log files can not be created, use stderr and
  stdout instead.

upnp/src/api/upnpdebug.c
@@ -70,10 +70,12 @@ int UpnpInitLog(void)
 	ithread_mutex_init(&GlobalDebugMutex, NULL);
 	if (DEBUG_TARGET == 1) {
 		if ((ErrFileHnd = fopen(errFileName, "a")) == NULL) {
-			return -1;
+			fprintf(stderr, "Failed to open errFileName (%s): %s\n", errFileName, strerror(errno));
+			ErrFileHnd = stderr;
 		}
 		if ((InfoFileHnd = fopen(infoFileName, "a")) == NULL) {
-			return -1;
+			fprintf(stderr, "Failed to open infoFileName (%s): %s\n", infoFileName, strerror(errno));
+			InfoFileHnd = stdout;
 		}
 	}
 	return UPNP_E_SUCCESS;

commit fa2778374e738da83d11cf8026307432b2b11f15

debug: #include <errno.h> for errno
This fixes a build failure when ./configured --with-debug

Fixes: 0df404f9d644 ("Fix crash when debug is enabled")

upnp/src/api/upnpdebug.c
@@ -40,6 +40,7 @@
 #include "upnp.h"
 #include "upnpdebug.h"
 
+#include <errno.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>