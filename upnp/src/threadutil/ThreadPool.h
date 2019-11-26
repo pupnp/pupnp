421d5d2875a639b7c037ceadb8eec75d70d5346e
Marvin Scholz <epirat07@gmail.com>
2019-11-26 01:22:12 +0100

    Do not redeclare timezone struct if already defined in Mingw

--- upnp/src/threadutil/ThreadPool.h
+++ upnp/src/threadutil/ThreadPool.h
@@ -47,12 +47,15 @@
 
 #ifdef _WIN32
 	#include <time.h>
-	struct timezone
-	{
-		int  tz_minuteswest; /* minutes W of Greenwich */
-		int  tz_dsttime;     /* type of dst correction */
-	};
-	int gettimeofday(struct timeval *tv, struct timezone *tz);
+
+	#ifndef _TIMEZONE_DEFINED
+		struct timezone
+		{
+			int  tz_minuteswest; /* minutes W of Greenwich */
+			int  tz_dsttime;     /* type of dst correction */
+		};
+		int gettimeofday(struct timeval *tv, struct timezone *tz);
+	#endif
 #else /* _WIN32 */
 	#include <sys/param.h>
 	#include <sys/time.h> /* for gettimeofday() */
