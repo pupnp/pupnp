fccb695f3eac121e7c7ba482feca89b57f1cd419
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2021-03-14 00:20:10 -0300

    Fix formatting

--- upnp/src/inc/handle_table.h
+++ upnp/src/inc/handle_table.h
@@ -3,9 +3,9 @@
 
 #define HANDLE_TABLE_MAX_NUM_ELEMENTS 200
 
-typedef struct s_handle_table {
-	void *handle[HANDLE_TABLE_MAX_NUM_ELEMENTS];
+typedef struct s_handle_table
+{
+        void *handle[HANDLE_TABLE_MAX_NUM_ELEMENTS];
 } handle_table_t;
 
 #endif /* HANDLE_TABLE_H */
-
