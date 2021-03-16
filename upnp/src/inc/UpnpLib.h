7ed9cd9b3ca5a280fc46fe70177fd79db4da22e3
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2021-03-16 05:35:27 -0300

    Revert "Trying to fix the build by hand"
    This reverts commit 0cef2b33e7497094e56fa6052d1af8bcf39b680f.

--- upnp/src/inc/UpnpLib.h
+++ upnp/src/inc/UpnpLib.h
@@ -29,9 +29,7 @@ typedef struct s_UpnpLib UpnpLib;
 #include "upnpapi.h"
 #include "xml_alias.h"
 
-#ifdef UPNP_ENABLE_OPEN_SSL
 #include <openssl/ssl.h>
-#endif
 #include <stdlib.h> /* for size_t */
 
 #ifdef __cplusplus
