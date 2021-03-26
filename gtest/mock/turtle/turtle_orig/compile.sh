d01f5e43d471809aa0ab91067687f19e82be7b31
Ingo <ingo@hoeft-online.de>
2021-03-26 18:00:31 +0100

    Make gtests running with new user interface
    Improved the gtests and make them running with the new user interface
    with the UpnpLib structure.

--- gtest/mock/turtle/turtle_orig/compile.sh
+++
@@ -1,2 +0,0 @@
-BUILD_DIR="$HOME/devel/pupnp-dev/pupnp"
-/usr/bin/g++ -std=c++11 -pedantic-errors -Wall -o mock_turtle_test.a -I$BUILD_DIR/googletest-src/googletest/include -I$BUILD_DIR/googletest-src/googlemock/include mock_turtle_test.cpp $BUILD_DIR/lib/libgtestd.a $BUILD_DIR/lib/libgmockd.a -lpthread
--- gtest/tools/compile.sh
+++
@@ -1,2 +0,0 @@
-BUILD_DIR="$HOME/devel/pupnp-dev/pupnp"
-g++ -std=c++11 -pedantic-errors -Wall -o test_tools.a -I"$BUILD_DIR"/googletest-src/googletest/include -I"$BUILD_DIR"/googletest-src/googlemock/include test_tools.cpp "$BUILD_DIR"/lib/libgtestd.a "$BUILD_DIR"/lib/libgmockd.a -lpthread
