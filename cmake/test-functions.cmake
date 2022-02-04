bd3ab8e2cfe96dd6266d65abd1e3672c200fe204
Vollstrecker <werner@vollstreckernet.de>
2022-02-04 12:29:38 +0100

    Added MinGW to check.

--- cmake/test-functions.cmake
+++ cmake/test-functions.cmake
@@ -95,7 +95,7 @@ function (UPNP_addUnitTest testName sourceFile)
 			COMMAND ${testName}
 		)
 
-		if (MSVC OR MSYS)
+		if (MSVC OR MSYS OR MINGW)
 			UPNP_findTestEnv (${testName} TEST_ENV)
 message(STATUS "Setting test-env to ${TEST_ENV}")
 			set_tests_properties (${testName} PROPERTIES
