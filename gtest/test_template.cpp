942fd8fda410d7e257ce0cd43eed744051bdfab4
Vollstrecker <werner@vollstreckernet.de>
2022-02-05 11:15:55 +0100

    Spaces -> Tab .. and removed some doubled empty lines.

--- gtest/test_template.cpp
+++ gtest/test_template.cpp
@@ -6,31 +6,31 @@
 
 // for TestSuites linked against the static C library
 extern "C" {
-    //#include "upnp.h"
+	//#include "upnp.h"
 }
 
 // two macros to compare values in a range
-#define EXPECT_IN_RANGE(VAL, MIN, MAX) \
-    EXPECT_GE((VAL), (MIN));           \
-    EXPECT_LE((VAL), (MAX))
+#define EXPECT_IN_RANGE(VAL, MIN, MAX)	\
+	EXPECT_GE((VAL), (MIN));			\
+	EXPECT_LE((VAL), (MAX))
 
-#define ASSERT_IN_RANGE(VAL, MIN, MAX) \
-    ASSERT_GE((VAL), (MIN));           \
-    ASSERT_LE((VAL), (MAX))
+#define ASSERT_IN_RANGE(VAL, MIN, MAX)	\
+	ASSERT_GE((VAL), (MIN));			\
+	ASSERT_LE((VAL), (MAX))
 
 
 // simple testsuite without fixtures
 //----------------------------------
 TEST(EmptyTestSuite, empty_gtest)
 {
-    //GTEST_SKIP();
-    //GTEST_SKIP_("to show this feature");
+	//GTEST_SKIP();
+	//GTEST_SKIP_("to show this feature");
 
-    // SKIP on Github Actions
-    char* github_action = std::getenv("GITHUB_ACTIONS");
-    if(github_action) { GTEST_SKIP()
-        << "  to show this feature";
-    }
+	// SKIP on Github Actions
+	char* github_action = std::getenv("GITHUB_ACTIONS");
+	if(github_action) { GTEST_SKIP()
+		<< "  to show this feature";
+	}
 }
 
 
@@ -38,37 +38,37 @@ TEST(EmptyTestSuite, empty_gtest)
 //------------------------
 class EmptyFixtureTestSuite : public ::testing::Test
 {
-    protected:
-    // You can remove any or all of the following functions if their bodies would
-    // be empty.
+	protected:
+	// You can remove any or all of the following functions if their bodies would
+	// be empty.
 
-    EmptyFixtureTestSuite()
-    {
-        // You can do set-up work for each test here.
-    }
+	EmptyFixtureTestSuite()
+	{
+		// You can do set-up work for each test here.
+	}
 
-    ~EmptyFixtureTestSuite() override
-    {
-        // You can do clean-up work that doesn't throw exceptions here.
-    }
+	~EmptyFixtureTestSuite() override
+	{
+		// You can do clean-up work that doesn't throw exceptions here.
+	}
 
-        // If the constructor and destructor are not enough for setting up
-        // and cleaning up each test, you can define the following methods:
+		// If the constructor and destructor are not enough for setting up
+		// and cleaning up each test, you can define the following methods:
 
-    void SetUp() override
-    {
-        // Code here will be called immediately after the constructor (right
-        // before each test). Have attention to the uppercase 'U' of SetUp().
-    }
+	void SetUp() override
+	{
+		// Code here will be called immediately after the constructor (right
+		// before each test). Have attention to the uppercase 'U' of SetUp().
+	}
 
-    void TearDown() override
-    {
-        // Code here will be called immediately after each test (right
-        // before the destructor).
-    }
+	void TearDown() override
+	{
+		// Code here will be called immediately after each test (right
+		// before the destructor).
+	}
 
-    // Class members declared here can be used by all tests in the test suite
-    // for Foo.
+	// Class members declared here can be used by all tests in the test suite
+	// for Foo.
 };
 
 TEST_F(EmptyFixtureTestSuite, empty_gtest_with_fixture)
@@ -77,6 +77,6 @@ TEST_F(EmptyFixtureTestSuite, empty_gtest_with_fixture)
 
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
