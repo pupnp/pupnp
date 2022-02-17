6d3757549b9a8b938e9c1e5339d7e188a3c57911
Vollstrecker <werner@vollstreckernet.de>
2022-02-17 15:46:48 +0100

    Add section description and fix for strncpy

--- ixml/inc/posix_overwrites.h
+++ ixml/inc/posix_overwrites.h
@@ -2,6 +2,7 @@
 #define POSIX_OVERWRTIES_H
 #ifdef _WIN32
 
+	/* POSIX names for functions */
 	#define fileno _fileno
 	#define unlink _unlink
 	#define strcasecmp _stricmp
@@ -9,6 +10,9 @@
 	#define stricmp _stricmp
 	#define strncasecmp strnicmp
 	#define strnicmp _strnicmp
+
+	/* Secure versions of functions */
+	#define strncpy(arg1,arg2,arg3) strncpy_s(arg1,arg3,arg2,arg3)
 
 #endif /* _WIN32 */
 #endif /* POSIX_OVERWRTIES_H */
