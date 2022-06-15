7e4c1d836e0b0518e05a5bcb03877b0c8138f817
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2022-06-15 11:32:51 -0300

    Add missing "site" folder

---
+++ site/app.js
@@ -0,0 +1,10 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('index.html').pipe(res)
+})
+
+server.listen(process.env.PORT || 3000)
+
