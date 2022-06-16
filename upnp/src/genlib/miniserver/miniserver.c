1c35d209e75c83563b09bb61c94cd3295a42f9b4
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2022-06-16 12:17:58 -0300

    clang-format

--- upnp/src/genlib/miniserver/miniserver.c
+++ upnp/src/genlib/miniserver/miniserver.c
@@ -163,30 +163,30 @@ static int host_header_is_numeric(
 	char *s;
 	(void)p;
 
-        /* Remove the port part. */
-        s = host_port + host_port_size - 1;
-        while (s != host_port && *s != ']' && *s != ':') {
-                --s;
-        }
-        if (*s == ':') {
-                *s = 0;
-        } else {
-                s = host_port + host_port_size;
-        }
+	/* Remove the port part. */
+	s = host_port + host_port_size - 1;
+	while (s != host_port && *s != ']' && *s != ':') {
+		--s;
+	}
+	if (*s == ':') {
+		*s = 0;
+	} else {
+		s = host_port + host_port_size;
+	}
 
-        /* Try IPV4 */
-        rc = inet_pton(AF_INET, host_port, &addr);
-        if (rc == 1) {
-                goto ExitFunction;
-        }
-        /* Try IPV6 */
-        /* Check for and remove the square brackets. */
-        if (strlen(host_port) < 3 || host_port[0] != '[' || *(s - 1) != ']') {
-                rc = 0;
-                goto ExitFunction;
-        }
-        *(s - 1) = '\0';
-        rc = inet_pton(AF_INET6, host_port + 1, &addr) == 1;
+	/* Try IPV4 */
+	rc = inet_pton(AF_INET, host_port, &addr);
+	if (rc == 1) {
+		goto ExitFunction;
+	}
+	/* Try IPV6 */
+	/* Check for and remove the square brackets. */
+	if (strlen(host_port) < 3 || host_port[0] != '[' || *(s - 1) != ']') {
+		rc = 0;
+		goto ExitFunction;
+	}
+	*(s - 1) = '\0';
+	rc = inet_pton(AF_INET6, host_port + 1, &addr) == 1;
 
 ExitFunction:
 	return rc;
