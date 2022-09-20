e7eafae6db6df1e38022de8d460e3cac62990403
Rex Cheung <93339127+rexcheung21@users.noreply.github.com>
2022-09-20 12:23:42 +0800

    Fix #412
    Skip leading UTF-8 BOM in XML document

--- ixml/src/ixmlparser.c
+++ ixml/src/ixmlparser.c
@@ -74,6 +74,7 @@ static const char *CDSTART = "<![CDATA[";
 static const char *CDEND = "]]>";
 static const char *DEC_NUMBERS = "0123456789";
 static const char *HEX_NUMBERS = "0123456789ABCDEFabcdef";
+static const char *UTF8_BOM = "\xef\xbb\xbf";
 
 typedef struct char_info
 {
@@ -592,6 +593,19 @@ static int Parser_skipString(
 }
 
 /*!
+ * \brief Skip UTF-8 byte order mark
+ */
+static void Parser_skipBom(
+	/*! [in] The XML parser. */
+	Parser *xmlParser)
+{
+	size_t bom_len = strlen(UTF8_BOM);
+
+	if (strncmp(xmlParser->curPtr, UTF8_BOM, bom_len) == 0)
+		xmlParser->curPtr += bom_len;
+}
+
+/*!
  * \brief Skip white spaces.
  */
 static void Parser_skipWhiteSpaces(
@@ -702,6 +716,7 @@ static int Parser_skipProlog(
 		return IXML_FAILED;
 	}
 
+	Parser_skipBom(xmlParser);
 	Parser_skipWhiteSpaces(xmlParser);
 
 	if (strncmp(xmlParser->curPtr, (char *)XMLDECL, strlen(XMLDECL)) == 0) {
