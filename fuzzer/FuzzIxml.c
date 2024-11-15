80e5ea7ef6815f82771312269bf76fb678a3d70f
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2024-11-15 15:06:46 -0300

    fuzzer: clang-format on FuzzIxml.c

--- fuzzer/FuzzIxml.c
+++ fuzzer/FuzzIxml.c
@@ -1,56 +1,57 @@
-#include <stdio.h>
-#include <unistd.h>
+#include "ixml.h"
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "ixml.h"
+#include <unistd.h>
 
 #define kMinInputLength 10
 #define kMaxInputLength 5120
 
-int CheckXML(char *filename){
+int CheckXML(char *filename)
+{
 
-    int rc;
-    DOMString s;
-    IXML_Document *doc = NULL;
+	int rc;
+	DOMString s;
+	IXML_Document *doc = NULL;
 
-    rc = ixmlLoadDocumentEx(filename, &doc);
-    if (rc != IXML_SUCCESS) {
-        return rc;
-    }
+	rc = ixmlLoadDocumentEx(filename, &doc);
+	if (rc != IXML_SUCCESS) {
+		return rc;
+	}
 
-    s = ixmlPrintDocument(doc);
-    if (s == NULL || s[0] == '\0') {
-        ixmlDocument_free(doc);
-        return 1;
-    }
+	s = ixmlPrintDocument(doc);
+	if (s == NULL || s[0] == '\0') {
+		ixmlDocument_free(doc);
+		return 1;
+	}
 
-    ixmlFreeDOMString(s);
-    ixmlDocument_free(doc);
+	ixmlFreeDOMString(s);
+	ixmlDocument_free(doc);
 
-    return 0;
+	return 0;
 }
 
-extern int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+extern int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
+{
 
-    if (Size < kMinInputLength || Size > kMaxInputLength){
-        return 1;
-    }
-    
-    int ret;
-    char filename[256];
+	if (Size < kMinInputLength || Size > kMaxInputLength) {
+		return 1;
+	}
 
-    sprintf(filename, "/tmp/libfuzzer.%d", getpid());
-    FILE *fp = fopen(filename, "wb");
-    if (!fp) {
-        return 0;
-    }
+	int ret;
+	char filename[256];
 
-    fwrite(Data, Size, 1, fp);
-    fclose(fp);
+	sprintf(filename, "/tmp/libfuzzer.%d", getpid());
+	FILE *fp = fopen(filename, "wb");
+	if (!fp) {
+		return 0;
+	}
 
-    ret = CheckXML(filename);
-    unlink(filename);
-    return ret;
+	fwrite(Data, Size, 1, fp);
+	fclose(fp);
+
+	ret = CheckXML(filename);
+	unlink(filename);
+	return ret;
 }
-
