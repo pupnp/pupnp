af07e6d5bd1a85f25bf95004b16334d1786d3d1e
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2025-02-08 21:46:24 -0300

    FuzzIxml: Fix building process, uses one less directory

--- fuzzer/build.sh
+++ fuzzer/build.sh
@@ -1,14 +1,11 @@
 #!/bin/bash -eu
 
 compile() {
-
    cd build/
    make -j"$(nproc)"
-
 }
 
 build() {
-
    export CFLAGS="$1"
    export CXXFLAGS="$1"
    export LIB_FUZZING_ENGINE=-fsanitize=fuzzer
@@ -18,43 +15,41 @@ build() {
    # echo CFLAGS="${CFLAGS}"
    # echo CXXFLAGS="${CXXFLAGS}"
 
-   rm -rf build
-   mkdir -p build
-   cd build/
+   rm -rf "${BUILD_DIR}"
+   # cmake --fresh -DFUZZER=ON -DLIB_FUZZING_ENGINE="$LIB_FUZZING_ENGINE" -B build &&
+   cmake --fresh -DLIB_FUZZING_ENGINE="$LIB_FUZZING_ENGINE" -B "${BUILD_DIR}" &&
+      cmake --build "${BUILD_DIR}"
 
-   cmake --fresh -DFUZZER=ON -DLIB_FUZZING_ENGINE="$LIB_FUZZING_ENGINE" ../../.
-   make -j"$(nproc)"
-
-   cd fuzzer/
-
+   cd "${BUILD_DIR}"
    mkdir FuzzIxml_corpus
    mkdir FuzzIxml_seed_corpus
 
-   cp ../../../ixml/test/testdata/empty_attribute.xml FuzzIxml_seed_corpus/
-   cp ../../../ixml/test/testdata/p43pass1.xml FuzzIxml_seed_corpus/
-   cp ../../../ixml/test/testdata/xml.ascc.net/wf/zh-utf8-8.xml FuzzIxml_seed_corpus/
-   cp ../../../ixml/test/testdata/xml.ascc.net/wfns/zh-utf8-7.xml FuzzIxml_seed_corpus/
-   cp ../../../ixml/test/testdata/xmlconf/014.xml FuzzIxml_seed_corpus/
+   cp ../../ixml/test/testdata/empty_attribute.xml FuzzIxml_seed_corpus/
+   cp ../../ixml/test/testdata/p43pass1.xml FuzzIxml_seed_corpus/
+   cp ../../ixml/test/testdata/xml.ascc.net/wf/zh-utf8-8.xml FuzzIxml_seed_corpus/
+   cp ../../ixml/test/testdata/xml.ascc.net/wfns/zh-utf8-7.xml FuzzIxml_seed_corpus/
+   cp ../../ixml/test/testdata/xmlconf/014.xml FuzzIxml_seed_corpus/
 }
 
 run() {
-   DIR=build/fuzzer
-   #./$DIR/FuzzIxml $DIR/FuzzIxml_corpus/ $DIR/FuzzIxml_seed_corpus/ -detect_leaks=0
-   ./$DIR/FuzzIxml $DIR/FuzzIxml_corpus/ $DIR/FuzzIxml_seed_corpus/
+   #./"${BUILD_DIR}"/FuzzIxml "${BUILD_DIR}"/FuzzIxml_corpus/ "${BUILD_DIR}"/FuzzIxml_seed_corpus/ -detect_leaks=0
+   ./"${BUILD_DIR}"/FuzzIxml "${BUILD_DIR}"/FuzzIxml_corpus/ "${BUILD_DIR}"/FuzzIxml_seed_corpus/
+   #./"${BUILD_DIR}"/FuzzIxml "${BUILD_DIR}"/FuzzIxml_corpus/ "${BUILD_DIR}"/FuzzIxml_seed_corpus/ >/dev/null 2>&1
 }
 
 usage() {
    echo "usage: $0 ASan | UBSan | MSan | Run | compile"
 }
 
-export CC=clang
-export CXX=clang++
-
 if [ $# -eq 0 ]; then
    echo "Error: No arguments supplied"
    usage
    exit 1
 fi
+
+export CC=clang
+export CXX=clang++
+export BUILD_DIR=build
 
 if [ "$1" == "ASan" ]; then
    build "-g -O0 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=address -fsanitize-address-use-after-scope -fsanitize=fuzzer-no-link"
