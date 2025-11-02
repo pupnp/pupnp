b39799dd073d0ae04439271bf2f4f6d153f26648
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2025-11-01 23:00:20 -0300

    test-functions.cmake: Reformat

--- cmake/test-functions.cmake
+++ cmake/test-functions.cmake
@@ -1,173 +1,186 @@
-function (UPNP_addGTest testName sourceFile)
-	cmake_parse_arguments (PARSE_ARGV 2 "agm" "" "" "ADDITIONAL_INCLUDE_DIRS")
+function(UPNP_addGTest testName sourceFile)
+	cmake_parse_arguments(
+		PARSE_ARGV 2 "agm" "" "" "ADDITIONAL_INCLUDE_DIRS"
+	)
 
-	if (agm_UNPARSED_ARGUMENTS)
-		message (FATAL_ERROR "Additional Arg given to ${testName}: ${agm_UNPARSED_ARGUMENTS}")
+	if(agm_UNPARSED_ARGUMENTS)
+		message(
+			FATAL_ERROR
+				"Additional Arg given to ${testName}: ${agm_UNPARSED_ARGUMENTS}"
+		)
 	endif()
 
-	UPNP_addTestExecutable (${testName} ${sourceFile})
+	upnp_addtestexecutable(${testName} ${sourceFile})
 
-	if (UPNP_BUILD_SHARED)
-		if (agm_ADDITIONAL_INCLUDE_DIRS)
-			target_include_directories (${testName}
+	if(UPNP_BUILD_SHARED)
+		if(agm_ADDITIONAL_INCLUDE_DIRS)
+			target_include_directories(
+				${testName}
 				PRIVATE ${agm_ADDITIONAL_INCLUDE_DIRS}
 			)
-
 		endif()
 
-		target_link_libraries (${testName}
-			PRIVATE GTest::gmock
-		)
+		target_link_libraries(${testName} PRIVATE GTest::gmock)
 
-		gtest_add_tests (TARGET ${testName}
+		gtest_add_tests(
+			TARGET ${testName}
 			TEST_PREFIX test-upnp-
 			TEST_LIST GTEST_${testName}
 		)
 
-		if (MSVC)
-			UPNP_findTestEnv (${testName} TEST_ENV)
+		if(MSVC)
+			upnp_findtestenv(${testName} TEST_ENV)
 
-			set_tests_properties (${GTEST_${testName}} PROPERTIES
-				ENVIRONMENT "${TEST_ENV}"
+			set_tests_properties(
+				${GTEST_${testName}} PROPERTIES ENVIRONMENT
+								"${TEST_ENV}"
 			)
 		endif()
 
-		UPNP_findTestEnv (${testName} TEST_ENV)
+		upnp_findtestenv(${testName} TEST_ENV)
 	endif()
 
-	if (UPNP_BUILD_STATIC)
-		if (agm_ADDITIONAL_INCLUDE_DIRS)
-			target_include_directories (${testName}-static
+	if(UPNP_BUILD_STATIC)
+		if(agm_ADDITIONAL_INCLUDE_DIRS)
+			target_include_directories(
+				${testName}-static
 				PRIVATE ${agm_ADDITIONAL_INCLUDE_DIRS}
 			)
 		endif()
 
-		target_link_libraries (${testName}-static
-			PRIVATE GTest::gmock
-		)
+		target_link_libraries(${testName}-static PRIVATE GTest::gmock)
 
-		gtest_add_tests (TARGET ${testName}-static
+		gtest_add_tests(
+			TARGET ${testName}-static
 			TEST_PREFIX test-upnp-
 			TEST_SUFFIX -static
 		)
 	endif()
 endfunction()
 
-function (UPNP_addTestExecutable testName sourceFile)
-	if (UPNP_BUILD_SHARED)
-		add_executable (${testName}
-			${sourceFile}
-		)
+function(UPNP_addTestExecutable testName sourceFile)
+	if(UPNP_BUILD_SHARED)
+		add_executable(${testName} ${sourceFile})
 
-		target_link_libraries (${testName}
-			PRIVATE upnp_shared
-		)
+		target_link_libraries(${testName} PRIVATE upnp_shared)
 
-		if (HAVE_MACRO_PREFIX_MAP)
-			target_compile_options(${testName}
+		if(HAVE_MACRO_PREFIX_MAP)
+			target_compile_options(
+				${testName}
 				PRIVATE -fmacro-prefix-map=${CMAKE_SOURCE_DIR}/=
 			)
 		endif()
 	endif()
 
-	if (UPNP_BUILD_STATIC)
-		add_executable (${testName}-static
-			${sourceFile}
-		)
+	if(UPNP_BUILD_STATIC)
+		add_executable(${testName}-static ${sourceFile})
 
-		target_link_libraries (${testName}-static
-			PRIVATE upnp_static
-		)
+		target_link_libraries(${testName}-static PRIVATE upnp_static)
 
-		if (HAVE_MACRO_PREFIX_MAP)
-			target_compile_options(${testName}-static
+		if(HAVE_MACRO_PREFIX_MAP)
+			target_compile_options(
+				${testName}-static
 				PRIVATE -fmacro-prefix-map=${CMAKE_SOURCE_DIR}/=
 			)
 		endif()
 	endif()
 endfunction()
 
-function (UPNP_addUnitTest testName sourceFile)
-	UPNP_addTestExecutable (${testName} ${sourceFile})
+function(UPNP_addUnitTest testName sourceFile)
+	upnp_addtestexecutable(${testName} ${sourceFile})
 
-	if (UPNP_BUILD_SHARED)
-		add_test (NAME ${testName}
-			COMMAND ${testName}
+	if(UPNP_BUILD_SHARED)
+		add_test(NAME ${testName} COMMAND ${testName})
+
+		if(MSVC
+			OR MSYS
+			OR MINGW
+			OR CYGWIN
 		)
+			upnp_findtestenv(${testName} TEST_ENV)
 
-		if (MSVC OR MSYS OR MINGW OR CYGWIN)
-			UPNP_findTestEnv (${testName} TEST_ENV)
-
-			set_tests_properties (${testName} PROPERTIES
-				ENVIRONMENT "${TEST_ENV}"
+			set_tests_properties(
+				${testName} PROPERTIES ENVIRONMENT
+							"${TEST_ENV}"
 			)
 		endif()
 	endif()
 
-	if (UPNP_BUILD_STATIC)
-		add_test (NAME ${testName}-static
-			COMMAND ${testName}-static
-		)
+	if(UPNP_BUILD_STATIC)
+		add_test(NAME ${testName}-static COMMAND ${testName}-static)
 	endif()
 endfunction()
 
 # For MSVC toolchain only
-function (UPNP_findTestEnv testName resultVar)
-	UPNP_findTestLibs (${testName} ${resultVar})
-	set (tempEnv "PATH=")
+function(UPNP_findTestEnv testName resultVar)
+	upnp_findtestlibs(${testName} ${resultVar})
+	set(tempEnv "PATH=")
 
-	if (MSVC OR MINGW)
-		set (separator "\\\;")
+	if(MSVC OR MINGW)
+		set(separator "\\\;")
 	else()
-		set (separator ":")
+		set(separator ":")
 	endif()
 
-	foreach (entry IN ITEMS ${${resultVar}})
-		string (APPEND tempEnv "${entry}${separator}")
+	foreach(entry IN ITEMS ${${resultVar}})
+		string(APPEND tempEnv "${entry}${separator}")
 	endforeach()
 
-	string (APPEND tempEnv "$ENV{PATH}")
-	set (${resultVar} ${tempEnv} PARENT_SCOPE)
+	string(APPEND tempEnv "$ENV{PATH}")
+	set(${resultVar}
+		${tempEnv}
+		PARENT_SCOPE
+	)
 endfunction()
 
-function (UPNP_findTestLibs testName resultVar)
-	unset (linkLibs)
+function(UPNP_findTestLibs testName resultVar)
+	unset(linkLibs)
 
-	if (NOT TARGET ${testName})
-		set (interface TRUE)
+	if(NOT TARGET ${testName})
+		set(interface TRUE)
 	else()
-		get_property (interface
+		get_property(
+			interface
 			TARGET ${testName}
 			PROPERTY IMPORTED
 		)
 	endif()
 
-	if (NOT ${interface})
-		get_property (linkLibs
+	if(NOT ${interface})
+		get_property(
+			linkLibs
 			TARGET ${testName}
 			PROPERTY LINK_LIBRARIES
 		)
 
-		foreach (lib IN ITEMS ${linkLibs})
-			UPNP_findTestLibs (${lib} ${resultVar})
+		foreach(lib IN ITEMS ${linkLibs})
+			upnp_findtestlibs(${lib} ${resultVar})
 
-			if (NOT TARGET ${lib})
-				set (interface2 TRUE)
+			if(NOT TARGET ${lib})
+				set(interface2 TRUE)
 			else()
-				get_property (interface2
+				get_property(
+					interface2
 					TARGET ${lib}
 					PROPERTY IMPORTED
 				)
 			endif()
 
-			if (NOT ${interface2})
-				list (FIND ${resultVar} "$<TARGET_FILE_DIR:${lib}>" index)
-				if (${index} STREQUAL "-1")
-					list (APPEND ${resultVar} "$<TARGET_FILE_DIR:${lib}>")
+			if(NOT ${interface2})
+				list(FIND ${resultVar}
+					"$<TARGET_FILE_DIR:${lib}>" index
+				)
+				if(${index} STREQUAL "-1")
+					list(APPEND ${resultVar}
+						"$<TARGET_FILE_DIR:${lib}>"
+					)
 				endif()
 			endif()
 		endforeach()
 	endif()
 
-	set (${resultVar} ${${resultVar}} PARENT_SCOPE)
+	set(${resultVar}
+		${${resultVar}}
+		PARENT_SCOPE
+	)
 endfunction()
