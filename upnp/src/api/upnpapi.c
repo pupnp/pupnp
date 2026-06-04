pupnp/pupnp

commit c86d963abe6a40499461946a38dfaf6958961e9f

Bug fix in IN6_IS_ADDR_GLOBAL.
Changing IN6_IS_ADDR_GLOBAL to accept all IPv6 addresses which have a
2000::/3 prefix.
(cherry picked from commit 92c93a8010a70bc9b8d62db88648b5ed6de7794b)

upnp/src/api/upnpapi.c
@@ -79,8 +79,8 @@
 
 #ifndef IN6_IS_ADDR_GLOBAL
 #define IN6_IS_ADDR_GLOBAL(a) \
-		(((((__const uint8_t *) (a))[0] & htonl(0xff000000)) <= htonl(0x3f000000)   \
-				&& (((__const uint8_t *) (a))[0] & htonl(0xff000000)) >= htonl(0x20000000)))
+		((((__const uint32_t *) (a))[0] & htonl(0x70000000)) \
+		== htonl (0x20000000))
 #endif /* IS ADDR GLOBAL */
 
 #ifndef IN6_IS_ADDR_ULA

commit 65e714a8f4560c46d66738e47a0e6d26bf87d1a9

fix: change IN6_IS_ADDR_GLOBAL macro to accept all 2000::/3 addresses
The Global Unicast addresses assigned by IANA should be allocated in
2000::/3 address space according to the RFC 3513, i.e. three most
significant bits of address should be 001.

Current implementation checks for three less significant bits in most
significant nibble. This effectively marks a000::/4 addresses as GUA
addresses, while excludes 3000::/4 from GUA diapason.

Issue was introduced in the c86d963abe6a40499461946a38dfaf6958961e9f
commit.

Fix uses correct bitmask for GUA address comparison.

Signed-off-by: Nikolai Merinov <n.merinov@inango-systems.com>

upnp/src/api/upnpapi.c
@@ -93,7 +93,7 @@
 #ifndef IN6_IS_ADDR_GLOBAL
 	#define IN6_IS_ADDR_GLOBAL(a) \
 		((((__const uint32_t *)(a))[0] & \
-			 htonl((uint32_t)0x70000000)) == \
+			 htonl((uint32_t)0xe0000000)) == \
 			htonl((uint32_t)0x20000000))
 #endif /* IS ADDR GLOBAL */
 